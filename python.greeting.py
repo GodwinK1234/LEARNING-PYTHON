26fefd710aa669b8d38294065ea694b0a485d732
GodwinK123 <Kpanazogodwin123@gmail.com>
2022-08-31 06:57:15 +0000

    Added new file Python Greeting Code

---
+++ python.greeting.py
@@ -0,0 +1,12 @@
+import datetime
+CurrentTime = datetime.datetime.now()
+currentTime.hour
+def greet():
+       name =input("Hi, whats your name")
+       return greeting + name
+if currentTime .hour <12:
+   print('Good morning' +name)
+elif    12 <= currentTime.hour < 16:
+   print('Good afternoon'+name)
+else:
+    print('Good evening '+name)
